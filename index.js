f17f8818847870651f7a9c5cf0b8f6ba1702efc4
Michael <michaelmwendwamumo@gmail.com>
2023-04-02 22:41:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10000) + 1;
+
+const num3 = 10
+const num4 = 6
+const mod = num3 % num4
+
+const set = [2,13,17,20];
+let max = Math.max(...set);
+
